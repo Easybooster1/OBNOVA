a90bda689c672d60bc2173167a2c6d014c7f7ad8
Easybooster1 <143163184+Easybooster1@users.noreply.github.com>
2024-05-19 19:50:24 +1000

    Update dfsfs.py

--- dfsfs.py
+++ dfsfs.py
@@ -17,7 +17,7 @@ class App:
     def __init__(self, master):
         self.window = master
         self.window.title("Escoria")
-        self.version = "1.6"
+        self.version = "2.0"
 
         self.input_label = tk.Label(self.window, text="Введите текст:")
         self.input_label.pack()
@@ -37,28 +37,28 @@ class App:
         self.output_text = tk.Text(self.window, height=5, width=30)
         self.output_text.pack()
 
-        self.copy_button = tk.Button(self.window, text="Скопировать", command=self.copy_to_clipboard)
+        self.copy_button = tk.Button(self.window, text="Скопировать", command=self.copy_to_clipboard, bg="red")  # Красная кнопка
         self.copy_button.pack()
 
-        self.about_button = tk.Button(self.window, text="О программе", command=self.show_about)
+        self.about_button = tk.Button(self.window, text="О программе", command=self.show_about, bg="green")  # Зеленая кнопка
         self.about_button.pack()
 
         self.create_menu()
 
     def encrypt_text(self):
-        text = self.input_text.get("1.6", tk.END).upper().strip()
+        text = self.input_text.get("2.0", tk.END).upper().strip()
         encrypted_text = ''
         for char in text:
             if char in morse_code:
                 encrypted_text += morse_code[char] + ' '
         if encrypted_text:
-            self.output_text.delete("1.6", tk.END)
+            self.output_text.delete("2.0", tk.END)
             self.output_text.insert(tk.END, encrypted_text)
         else:
             messagebox.showinfo("Ошибка", "Некорректный ввод")
 
     def decrypt_text(self):
-        text = self.input_text.get("1.6", tk.END).strip()
+        text = self.input_text.get("2.0", tk.END).strip()
         decrypted_text = ''
         morse_code_reverse = {value: key for key, value in morse_code.items()}
         words = text.split(' / ')
@@ -69,13 +69,13 @@ class App:
                     decrypted_text += morse_code_reverse[char]
             decrypted_text += ' '
         if decrypted_text:
-            self.output_text.delete("1.6", tk.END)
+            self.output_text.delete("2.0", tk.END)
             self.output_text.insert(tk.END, decrypted_text)
         else:
             messagebox.showinfo("Ошибка", "Некорректный ввод")
 
     def copy_to_clipboard(self):
-        text = self.output_text.get("1.6", tk.END).strip()
+        text = self.output_text.get("2.0", tk.END).strip()
         if text:
             self.window.clipboard_clear()
             self.window.clipboard_append(text)
